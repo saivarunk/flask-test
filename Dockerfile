d0871d9f35b9a8f860362d557d651a709ccfc718
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-28 11:53:40 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.6.4-alpine3.7 AS build
+
+WORKDIR /app
+
+ADD requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
+
+FROM build as test
+
+ADD app.py /app/app.py
+ADD integration_tests.sh /app
+ADD integration_tests.py /app
+
+RUN /bin/sh "./integration_tests.sh"
+
+FROM test as runtime
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
---
+++ integration_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app import create_app
+
+class Unittest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_valid_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.get_json()['message'], 'Hello !')
+        self.assertEqual(response.get_json()['success'], True)
+        self.assertEqual(response.status_code, 200)
