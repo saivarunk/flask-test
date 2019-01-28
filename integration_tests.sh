e1b05bfb39f68343a7d518ff5af3c909d4ce2c4f
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-28 20:41:33 +0530

    added postgres dependencies

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,6 +2,12 @@
 
 cd flask-test-source && ls -l
 
+apk update \
+  && apk add --virtual build-deps gcc python3-dev musl-dev \
+  && apk add postgresql-dev \
+  && pip install psycopg2 \
+  && apk del build-deps
+
 pip install -r requirements.txt
 
 python -m unittest integration_tests
