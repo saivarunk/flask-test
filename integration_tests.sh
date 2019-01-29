10c1c9f6bdef33da34ba329634f28171338c89df
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-29 17:46:49 +0530

    updated script

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
 
-# echo "http://nl.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repository
-
-echo "Hello from script"
+# install python and dependencies
 
 apt-get install python3 && apt-get install python-pip
+
+export PYTHON_PIP_VERSION=19.0.1
+
+set -ex; \
+	\
+	savedAptMark="$(apt-mark showmanual)"; \
+	apt-get update; \
+	apt-get install -y --no-install-recommends wget; \
+	\
+	wget -O get-pip.py 'https://bootstrap.pypa.io/get-pip.py'; \
+	\
+	apt-mark auto '.*' > /dev/null; \
+	[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark; \
+	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
+	rm -rf /var/lib/apt/lists/*; \
+	\
+	python get-pip.py \
+		--disable-pip-version-check \
+		--no-cache-dir \
+		"pip==$PYTHON_PIP_VERSION" \
+	; \
+	pip --version; \
+	\
+	find /usr/local -depth \
+		\( \
+			\( -type d -a \( -name test -o -name tests \) \) \
+			-o \
+			\( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
+		\) -exec rm -rf '{}' +; \
+	rm -f get-pip.py
 
 echo "Python version"
 python3 -V
