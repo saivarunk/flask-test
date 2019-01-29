c0cda92790dffb98df5d18c08b33fcfd59ebecb9
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-29 16:40:45 +0530

    updated scripts

--- integration_tests.sh
+++ integration_tests.sh
@@ -21,13 +21,13 @@ pwd
 
 cd flask-test-source && ls -l
 
-./setup.sh
+# ./setup.sh
 
-apk update \
-  && apk add --virtual build-deps gcc python3-dev musl-dev \
-  && apk add postgresql-dev \
-  && pip install psycopg2 \
-  && apk del build-deps
+# apk update \
+#   && apk add --virtual build-deps gcc python3-dev musl-dev \
+#   && apk add postgresql-dev \
+#   && pip install psycopg2 \
+#   && apk del build-deps
 
 pip install -r requirements.txt
 
