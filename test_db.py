73299f61c1333a50f99dfac6c89a00a08efcda1e
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-29 18:41:10 +0530

    updated db script

--- test_db.py
+++ test_db.py
@@ -6,7 +6,7 @@ def postgres_test():
         conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgres'")
         conn.close()
         return True
-    except Exception e:
+    except Exception as e:
         print(e)
         return False
 
