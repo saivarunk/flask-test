359fcb09630d996a29db3e01f5e505b7672ecbaa
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-29 16:22:06 +0530

    Updated scripts

--- integration_tests.sh
+++ integration_tests.sh
@@ -17,14 +17,14 @@ echo "Hello from script"
 # Enable and start postgresql server
 # rc-update add postgresql
 
-# cd flask-test-source && ls -l
+cd flask-test-source && ls -l
 
-# apk update \
-#   && apk add --virtual build-deps gcc python3-dev musl-dev \
-#   && apk add postgresql-dev \
-#   && pip install psycopg2 \
-#   && apk del build-deps
+apk update \
+  && apk add --virtual build-deps gcc python3-dev musl-dev \
+  && apk add postgresql-dev \
+  && pip install psycopg2 \
+  && apk del build-deps
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# python -m unittest integration_tests
+python -m unittest integration_tests
