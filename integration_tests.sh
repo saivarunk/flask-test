ca6d0d9fdaa6c72dd7ab027da740782ff930530d
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-30 10:54:32 +0530

    Updated script

--- integration_tests.sh
+++ integration_tests.sh
@@ -6,36 +6,6 @@ apt-get update && apt-get install -y python3 && apt-get install -y python-pip
 
 apt-get install -y curl
 
-# export PYTHON_PIP_VERSION=19.0.1
-
-# set -ex; \
-# 	\
-# 	savedAptMark="$(apt-mark showmanual)"; \
-# 	apt-get update; \
-# 	apt-get install -y --no-install-recommends wget; \
-# 	\
-# 	wget -O get-pip.py 'https://bootstrap.pypa.io/get-pip.py'; \
-# 	\
-# 	apt-mark auto '.*' > /dev/null; \
-# 	[ -z "$savedAptMark" ] || apt-mark manual $savedAptMark; \
-# 	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
-# 	rm -rf /var/lib/apt/lists/*; \
-# 	\
-# 	python get-pip.py \
-# 		--disable-pip-version-check \
-# 		--no-cache-dir \
-# 		"pip==$PYTHON_PIP_VERSION" \
-# 	; \
-# 	pip --version; \
-# 	\
-# 	find /usr/local -depth \
-# 		\( \
-# 			\( -type d -a \( -name test -o -name tests \) \) \
-# 			-o \
-# 			\( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
-# 		\) -exec rm -rf '{}' +; \
-# 	rm -f get-pip.py
-
 curl 'https://bootstrap.pypa.io/get-pip.py' --output get-pip.py 
 
 python3 get-pip.py
@@ -47,6 +17,8 @@ pip install -r requirements.txt
 apt-get install -y python3-dev
 apt-get install -y postgresql-dev
 pip install psycopg2
+
+/etc/init.d/postgresql start
 
 python3 test_db.py
 
