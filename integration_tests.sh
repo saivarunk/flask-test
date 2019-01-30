01ae9c870ea55558eea25e3023bc5d159012c8ed
Varun Kruthiventi <saivarunk@gmail.com>
2019-01-30 11:27:08 +0530

    updated scripts

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,23 +2,23 @@
 
 # install python and dependencies
 
-apt-get update && apt-get install -y python3 && apt-get install -y python-pip
+#apt-get update && apt-get install -y python3 && apt-get install -y python-pip
 
-apt-get install -y curl
+#apt-get install -y curl
 
-curl 'https://bootstrap.pypa.io/get-pip.py' --output get-pip.py 
+#curl 'https://bootstrap.pypa.io/get-pip.py' --output get-pip.py 
 
-python3 get-pip.py
+#python3 get-pip.py
+
+service postgresql start
 
 cd flask-test-source && ls -l
 
 pip install -r requirements.txt
 
-apt-get install -y python3-dev
-apt-get install -y postgresql-dev
-pip install psycopg2
-
-/etc/init.d/postgresql start
+#apt-get install -y python3-dev
+#apt-get install -y postgresql-dev
+#pip install psycopg2
 
 python3 test_db.py
 
